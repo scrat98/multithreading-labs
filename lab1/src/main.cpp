e4bdd4c5882920d7f03973e12fcee0e3fc8b427d
Artem Golovko <artem.golovko@siemens.com>
2021-03-22 19:20:33 +0300

    lab1: add random tests

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -3,12 +3,27 @@
 #include "utils.h"
 
 int main() {
-    Matrix matrix = {
-            {1, 2, 8.8, 9.9},
-            {3.0, 4.0, 3.3, 4.4},
-            {2.0, 5.2, 2.2, 5.5},
-            {6.3, 7.8, 6.3, 7.8}
-    };
+//    Matrix matrix = {
+//            {1,   2,   8.8, 9.9},
+//            {3.0, 4.0, 3.3, 4.4},
+//            {2.0, 5.2, 2.2, 5.5},
+//            {6.3, 7.8, 6.3, 7.8}
+//    };
+    srand(time(NULL));
+    int matrixSize = 7;
+    auto matrix = Matrix(matrixSize);
+    for (int i = 0; i < matrixSize; ++i) {
+        matrix[i].resize(matrixSize);
+        for (int j = 0; j < matrixSize; ++j) {
+            matrix[i][j] = 10.0 * rand() / RAND_MAX;
+        }
+    }
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix.size(); j++) {
+            std::cout << matrix[i][j] << ' ';
+        }
+        std::cout << std::endl;
+    }
     auto detCalculator = DeterminantCalculator::withOMP(omp_sched_auto, 8);
 
     float det;
