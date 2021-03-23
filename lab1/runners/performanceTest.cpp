81541f7e5914599f2d303a3e3ca81619ceed8a30
Artem Golovko <artem.golovko@siemens.com>
2021-03-23 14:44:12 +0300

    lab1: performance test results to file

--- lab1/runners/performanceTest.cpp
+++ lab1/runners/performanceTest.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <random>
+#include <map>
 #include "../src/utils.h"
 #include "../src/DeterminantCalculator.hpp"
 
@@ -21,8 +22,9 @@ Matrix *getRandomMatrix(int matrixSize) {
 
 int main() {
     std::vector<int> numberOfThreadsToTest = {0, 1, 2, 4, 6, 8, 16};
+    std::map<int, std::map<int, long long>> results;
 
-    for (int matrixSize = 10; matrixSize < 2500; matrixSize += 10) {
+    for (int matrixSize = 10; matrixSize < 1000; matrixSize += 10) {
         auto matrix = getRandomMatrix(matrixSize);
         std::printf("Matrix size: %d\n", matrixSize);
 
@@ -30,8 +32,31 @@ int main() {
             auto det = DeterminantCalculator::withOMP(numberOfThreads);
             auto elapsedTime = measureTimeMillis([&] { det->calculate(*matrix); });
             std::printf("Threads: %d, Time: %ld\n", numberOfThreads, elapsedTime);
+            results[matrixSize][numberOfThreads] = elapsedTime;
         }
+
+        auto det = DeterminantCalculator::withoutOMP();
+        auto elapsedTime = measureTimeMillis([&] { det->calculate(*matrix); });
+        std::printf("Threads: -1, Time: %ld\n", elapsedTime);
+        results[matrixSize][-1] = elapsedTime;
 
         delete matrix;
     }
+
+    auto resFile = fopen("result.csv", "w");
+    std::fprintf(resFile, "Matrix size");
+    for (auto const &res: results.begin()->second) {
+        std::fprintf(resFile, ", thread-<%d>", res.first);
+    }
+    std::fprintf(resFile, "\n");
+
+    for (auto const&[matrixSize, result]: results) {
+        std::fprintf(resFile, "%d", matrixSize);
+        for (auto const &res: result) {
+            std::fprintf(resFile, ", %lld", res.second);
+        }
+        std::fprintf(resFile, "\n");
+    }
+
+    return 0;
 }
