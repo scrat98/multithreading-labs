cc5d1e6a90cb778290c64cb6d0cdd0da79eb73f8
Artem Golovko <artem.golovko@siemens.com>
2021-04-12 02:58:29 +0300

    lab2: add cli runner

--- lab2/runners/main.cpp
+++ lab2/runners/main.cpp
@@ -1,21 +1,40 @@
+#include <cstdio>
 #include <string>
 #include "../src/PNMReaderWriter.hpp"
 #include "../src/PNMAutoBrightener.hpp"
 
-int main(int argc, char *argv[]) {
-    std::string rootFolder = "/home/ct/projects/multithreading-labs/lab2/images";
-    std::string imageType = "pgm";
-    std::string imageName = "baboon";
-    std::string imageInputPath = rootFolder + "/" + imageType + "/" + imageName + "." + imageType;
-    std::string imageOutputPath = rootFolder + "/" + imageType + "/" + imageName + "-new." + imageType;
+void run(const std::string &inFilePath, const std::string &outFilePath, int numOfThreads) {
+    bool useOmp = numOfThreads != -1;
+    PNMAutoBrightener *autoBrightener;
+    if (useOmp) {
+        if (numOfThreads < 0) {
+            throw std::invalid_argument("Wrong number of threads. Must be -1(omp disabled), 0(default) or greater than.");
+        }
+        autoBrightener = PNMAutoBrightener::withOMP(numOfThreads);
+    } else {
+        autoBrightener = PNMAutoBrightener::withoutOMP();
+    }
 
-    auto imageData = PNMReaderWriter::readFromFile(imageInputPath);
-    auto autoBrightener = PNMAutoBrightener::withoutOMP();
+    auto imageData = PNMReaderWriter::readFromFile(inFilePath);
     autoBrightener->correctImage(*imageData);
-    PNMReaderWriter::writeToFile(imageOutputPath, *imageData);
+    PNMReaderWriter::writeToFile(outFilePath, *imageData);
 
     delete imageData;
     delete autoBrightener;
+}
 
+int main(int argc, char *argv[]) {
+    try {
+        if (argc != 4) {
+            throw std::invalid_argument("Wrong number of arguments. Must be in format <in file> <out file> <number of threads>");
+        }
+        auto inFileName = std::string(argv[1]);
+        auto outFileName = std::string(argv[2]);
+        int numOfThreads = atoi(argv[3]);
+        run(inFileName, outFileName, numOfThreads);
+    } catch (const std::exception &e) {
+        std::fprintf(stderr, "The program was aborted: %s", e.what());
+        return 1;
+    }
     return 0;
 }
